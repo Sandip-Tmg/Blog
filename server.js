155a8eb39e5b3898ccf8f6c6c8b551253a2f7e53
Sandip Tamang <65627580+Sandip-Tmg@users.noreply.github.com>
2022-04-05 22:04:59 +0100

    server file for our app

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const articleRouter = require("./routes/article");
+const homeRouter = require("./routes/index");
+// connecting to database
+mongoose.connect(
+  "mongodb://localhost/Blog",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Database connected");
+  }
+);
+
+// setting up the engine to ejs template
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+// home route of our app 
+app.use("/" , homeRouter);
+
+// it uses a routes articles.js file for our article route
+app.use("/articles", articleRouter);
+
+app.listen(3000, () => {
+  console.log("Server running on 3000 port");
+});
